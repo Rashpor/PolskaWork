aa5246d047f56bd773cb893417234fd6673be0e5
Peter Nesterenko <rashpor@gmail.com>
2022-01-22 14:50:54 +0300

    Добавьте файлы проекта.

---
+++ PolskaTest1/ProgramTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PolskaWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Polska;
+
+namespace PolskaWork.Tests
+{
+
+
+    //первый тест  скорее интеграционный тест, он проверяет программу целиком, обращаясь к библиотеке с классом.
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void UseCalcTest()
+        {
+            
+            string expression = " - * / 15 - 7 + 1 1 3 + 2 + 1 1";
+            Prefix prefix = new Prefix(expression);
+            string expected = "5";
+
+            decimal actual = prefix.Calc();
+
+            Assert.AreEqual(expected, actual.ToString(), "Считает не верно!");
+        }
+    }
+
+
+    [TestClass()]
+    public class Prefix1
+    {
+        // список доступных операций
+        private List<char> operations = new List<char> { '+', '-', '*', '/' };
+
+        [TestMethod()]
+        public void GetStringNumberTest()
+        {
+            string expr = " - * / 15 - 7 + 1 1 3 + 2 + 1 1";
+            Prefix prefix = new Prefix(expr);
+            int pos = 8;
+            string expected = "15";
+
+            string actual = prefix.GetStringNumber(expr, ref pos);
+
+            Assert.AreEqual(expected, actual, "Считает не верно!");
+        }
+
+        [TestMethod()]
+        public void GetStringNumberTest2()
+        {
+            string expr = " - * / 15 - 7 + 1 1 3 + 2 + 1 1";
+            Prefix prefix = new Prefix(expr);
+            int pos = 2;
+            string expected = "";
+
+            string actual = prefix.GetStringNumber(expr, ref pos);
+
+            Assert.AreEqual(expected, actual, "Считает не верно!");
+        }
+
+        [TestMethod()]
+        public void CalcTest()
+        {
+            string expr = " - * / 15 - 7 + 1 1 3 + 2 + 1 1";
+            Prefix prefix = new Prefix(expr);
+            decimal expected = 5;
+
+            decimal actual = prefix.Calc();
+
+            Assert.AreEqual(expected,actual,"Считает не верно!");
+        }
+
+    }
+}
---
+++ PolskaWork/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Polska;
+
+namespace PolskaWork
+{
+    public class Program
+    {
+
+        public static void UseCalc()
+        {
+            try
+            {
+                //тестовые строки
+                //string expression1 = " - * / 15 - 7 + 1 1 3 + 2 + 1 1";
+                //string expression2 = "+ 3 4";
+
+                Console.WriteLine("Введите выражение в польской (префиксной) нотации");
+                Console.WriteLine("Поддерживаются сложение '+', вычитание '-', умножение '*' и деление '/'");
+                Console.WriteLine("Десятичные дроби указывайте через ','");
+                string expression = Console.ReadLine();
+
+                Prefix prefix = new(expression);
+
+                
+
+                Console.WriteLine("Введена строка " + prefix.prefixExpr);
+                Console.WriteLine(expression + " = " + prefix.Calc());
+                Console.WriteLine();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine();
+            }
+
+            KeepWorking();
+        }
+
+        static void KeepWorking() 
+        {
+            //переменная для управления
+            string x;
+            Console.WriteLine("Введите 1 для ввода нового выражения");
+            Console.WriteLine("Введите 2 чтобы закрыть калькулятор");
+
+            //Уточняем будет ли пользователь вводить новое выражение, или закрываем приложение
+            x = Console.ReadLine();
+            if (x == "1")
+            {
+                UseCalc();
+            }
+            else if (x == "2")
+            {
+                return;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            KeepWorking();
+        }
+
+
+
+    }
+}
